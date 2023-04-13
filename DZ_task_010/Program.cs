083ff15a98daaad13c87e1135beb477508b84bf4
Evgen <usachev231079@gmail.com>
2023-04-13 23:12:49 +0500

    DZ_task_010 new

--- DZ_task_010/Program.cs
+++ DZ_task_010/Program.cs
@@ -8,5 +8,5 @@ int N = int.Parse(Console.ReadLine() ?? "");
 int a1 = N / 10;
 int a2 = a1 % 10;
 
-Console.WriteLine ($"Второе цифра числа {N}  -> {a2}");
+Console.WriteLine ($"Вторая цифра числа {N}  -> {a2}");
 
