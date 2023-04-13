5d1282f7785a8e8c8dfebd242d6473c4263ae360
Evgen <usachev231079@gmail.com>
2023-04-14 00:53:05 +0500

    DZ_task_013

---
+++ DZ_task_013/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру 
+//заданного числа или сообщает, что третьей цифры нет.
+
+Console.Clear();
+Console.WriteLine ("Введите целое число: ");
+    
+int N = int.Parse(Console.ReadLine() ?? "");
+string str = N.ToString();
+if (N < 100)
+{
+    Console.WriteLine("Это не трехзначное число!!!");
+}
+else
+{
+    Console.WriteLine($"Третья цифра {N} -> {str[2]}");
+}
+
+
