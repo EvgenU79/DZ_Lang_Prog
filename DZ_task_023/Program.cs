d148e129be8f1430571d523edd04c1ed4b7b1b01
Evgen <usachev231079@gmail.com>
2023-04-21 00:13:00 +0500

    DZ_task_023

---
+++ DZ_task_023/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 23: Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+
+int num = GetNumberFromUser("Введите целое число: ", "Ошибка ввода! ");
+
+//////
+int GetNumberFromUser(string message, string ErrorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        if(int.TryParse(Console.ReadLine(), out int userNumber))
+        return userNumber;
+        Console.Write(ErrorMessage);
+    }
+}
+/////////////
+
+Console.Write("Таблица кубов:\n");
+for (int i=1;i<=num;i++)
+{
+int k = i*i*i;
+Console.Write($"{k}, ");
+}
