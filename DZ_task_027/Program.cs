a28a292f7c040d92e14ea6519285c2b5c8325579
Evgen <usachev231079@gmail.com>
2023-04-24 23:54:55 +0500

    DZ_task_027

---
+++ DZ_task_027/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+int num = GetNumberFromUser("Введите целое число: ", "Ошибка ввода!"); 
+int sum = GetSumDigit(num);
+Console.WriteLine($"Сумма чисел {num} равна -> {sum}");
+
+int GetNumberFromUser(string message, string errorMessage) 
+{     
+    while(true)     
+    {         
+    Console.Write(message);          
+        if(int.TryParse(Console.ReadLine(), out int userNumber))            
+        return userNumber;          
+        Console.WriteLine(errorMessage);     
+    } 
+} 
+
+int GetSumDigit(int num)
+{
+    int sum = 0;
+    while(num > 0)
+        {
+            sum += num % 10;
+            num = num / 10;
+        }
+    return sum;
+}
