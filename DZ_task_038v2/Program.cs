577a5499a5416d035ff1a1a7074b60cb7d7c715f
Evgen <usachev231079@gmail.com>
2023-04-28 01:05:09 +0500

    DZ_task_038_2

---
+++ DZ_task_038v2/Program.cs
@@ -0,0 +1,59 @@
+﻿// // Задача 38: Задайте массив вещественных чисел. 
+//Найдите разницу между максимальным и минимальным элементов массива.
+
+int length = GetNumberFromUser("Введите длину массива: ", "Ошибка ввода! ");
+
+double[] array = GetArray(length);
+Console.WriteLine(String.Join(", ", array));
+double maxElement = GetFindMax(array);
+double minElement = GetFindMin(array);
+
+Console.WriteLine($"Разница между MAX ({maxElement}) и MIN ({minElement}) = {maxElement - minElement}");
+
+//////
+int GetNumberFromUser(string message, string ErrorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out int userNumber))
+            return userNumber;
+        Console.Write(ErrorMessage);
+    }
+}
+
+double[] GetArray(int size)
+{
+    double[] res = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = Math.Round(new Random().NextDouble()*100);
+    }
+    return res;
+}
+
+double GetFindMax(double [] array)
+{
+ double maxElement = array[0];
+    for (int i = 1; i < length; i++) 
+    {
+        if (array[i] > maxElement) 
+        {
+            maxElement = array[i];
+        }
+    }
+    return maxElement;
+}
+
+double GetFindMin (double [] array)
+{
+ double minElement = array[0];
+    for (int i = 1; i < length; i++) 
+    {
+        if (array[i] < minElement) 
+        {
+            minElement = array[i];
+        }
+    }
+    return minElement;
+}
