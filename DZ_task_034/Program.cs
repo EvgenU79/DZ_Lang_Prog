000cb3650b2f45f21dcf45a61e5c93bc4c5244a3
Evgen <usachev231079@gmail.com>
2023-04-28 01:04:03 +0500

    DZ_task_034

---
+++ DZ_task_034/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] array = GetRandomArray(5); 
+PrintArray(array);
+int count = FindEvenNumber(array);
+Console.WriteLine($"В данном массиве {count} четных чисел");
+
+int[] GetRandomArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 999);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine(string.Join(" ", array));
+}
+
+int FindEvenNumber(int[] arr)
+{
+    int count = 0;
+    foreach (int el in arr)
+    {
+        if (el %2 ==0)
+            count++;
+    }
+    return count;
+}
