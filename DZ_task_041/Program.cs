1724548d3a0948d44226bb910c4b8ea0b8858cb1
Evgen <usachev231079@gmail.com>
2023-04-30 23:06:33 +0500

    DZ_task_041

---
+++ DZ_task_041/Program.cs
@@ -0,0 +1,41 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+int length = Prompt("Введите длину массива: ");
+int[] array = InputArray(length);
+
+Console.WriteLine(String.Join(", ", array));
+int count = FindPositivNumbers(array);
+Console.WriteLine($"Колличество положительных элементов = {count}");
+
+//////
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int[] InputArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Prompt($"Введите {i+1}-й элемент - ");
+    }
+    return array;
+}
+
+int FindPositivNumbers(int []array)
+{
+  int count = 0;
+  for (int i = 0; i < array.Length; i++)
+  {
+    if(array[i] > 0)
+    count++;
+  }
+    return count;
+}
