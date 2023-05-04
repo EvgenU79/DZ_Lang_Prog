1319166e10c506ad34662e3e47f0f008a109e3f7
Evgen <usachev231079@gmail.com>
2023-05-04 22:23:38 +0500

    DZ_task_050

---
+++ DZ_task_050/Program.cs
@@ -0,0 +1,51 @@
+﻿// //Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+Console.Write("Введите строку: ");
+int rows1 = Convert.ToInt32(Console.ReadLine()) - 1;
+Console.Write("Введите столбец: ");
+int colomnus2 = Convert.ToInt32(Console.ReadLine()) - 1;
+int rows = 3;
+int columns = 4;
+
+int[,] array = GetArray(rows, columns, 0, 99);
+PrintArray(array);
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+if (rows1 < 0 | rows1 > array.GetLength(0) - 1 | colomnus2 < 0 | colomnus2 > array.GetLength(1) - 1)
+{
+    Console.WriteLine("Элемент не существует  ");
+}
+else
+{
+    Console.WriteLine("Значение элемента массива = {0}", array[rows1, colomnus2]);
+}
+Console.ReadLine();
+
