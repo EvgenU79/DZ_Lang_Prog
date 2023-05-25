e195074e2c2592f9c1d84a127e328766ede05bc6
Evgen <usachev231079@gmail.com>
2023-05-25 22:34:33 +0500

    DZ_task_062

---
+++ DZ_task_062/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 62: Заполните спирально массив 4 на 4.
+
+int n = 4;
+int[,] sqareArray = new int[n, n];
+
+
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= sqareArray.GetLength(0) * sqareArray.GetLength(1))
+{
+  sqareArray[i, j] = temp;
+  temp++;
+  if (i <= j + 1 && i + j < sqareArray.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= sqareArray.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > sqareArray.GetLength(1) - 1)
+    j--;
+  else
+    i--;
+}
+
+WriteArray(sqareArray);
+
+void WriteArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      if (array[i,j] / 10 <= 0)
+      Console.Write($" {array[i,j]} ");
+
+
+      else Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine();
+  }
+}
+
