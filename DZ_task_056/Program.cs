a99345b5537bf0804fe35169a481460fca4ad249
Evgen <usachev231079@gmail.com>
2023-05-25 22:33:28 +0500

    DZ_task_056

---
+++ DZ_task_056/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. 
+//Напишите программу, которая будет находить строку с наименьшей суммой элементов
+
+Console.Clear();
+int m = GetNumberFromUser("Введите количество строк массива: ", "Ошибка ввода! ");
+int n = GetNumberFromUser("Введите количество столбцов массива: ", "Ошибка ввода! ");
+
+int[,] array = new int[m, n];
+GetArray(array);
+PrintArray(array);
+
+int minSumRow = 0;
+int sumLine = SumLineElements(array, 0);
+for (int i = 1; i < array.GetLength(0); i++)
+{
+    int tempSumLine = SumLineElements(array, i);
+    if (sumLine > tempSumLine)
+    {
+        sumLine = tempSumLine;
+        minSumRow = i;
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int SumLineElements(int[,] array, int i)
+{
+    int sumLine = array[i, 0];
+    for (int j = 1; j < array.GetLength(1); j++)
+    {
+        sumLine += array[i, j];
+    }
+    return sumLine;
+}
+
+int GetNumberFromUser(string message, string ErrorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out int userNumber))
+            return userNumber;
+        Console.Write(ErrorMessage);
+    }
+}
+
+void GetArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 9);
+        }
+    }
+}
+
+
+
+Console.WriteLine($"\n{minSumRow + 1} - строкa с наименьшей суммой ({sumLine}) элементов ");
