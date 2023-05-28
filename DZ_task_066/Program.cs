0f93e36b12d0a4a4e75645804b9898c8e8589801
Evgen <usachev231079@gmail.com>
2023-05-28 23:00:12 +0500

    DZ_task_066

---
+++ DZ_task_066/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8 -> 30
+
+Console.Clear();
+int firstNumb = GetValueFromUser("Введите первое число: ", "Ошибка ввода!Повторите попытку!");
+int secondNumb = GetValueFromUser("Введите второе число: ", "Ошибка ввода!Повторите попытку!");
+int result = GetSumm(firstNumb, secondNumb); 
+Console.WriteLine(result);
+
+////////////////////////////////////////  
+int GetValueFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out int userValue))
+            return userValue; Console.WriteLine(errorMessage);
+    }
+}
+int GetSumm(int numb1, int numb2)
+{
+    if (numb1 == numb2) return numb1;//завершаем цикл когда последнее число равно первому
+    return numb1 + GetSumm(numb1 + 1, numb2);
+}
+
